ff812ab5ec015d3f67f085702eb25880240356d1
Tin Tin <tint8140@gmail.com>
2021-04-06 05:41:27 -0400

    dfd

---
+++ app/app.js
@@ -0,0 +1,154 @@
+var userVarName= "Variable";
+var homeContent= ` <div class="home">
+<h1>What's Happening in Myanmar?</h1>
+<div>
+                <img src="images/leaders.jpg" alt="Myanmar Leaders">
+                <p><span>The 2021 Myanmar coup d'état began on the morning of 1 February 2021 </span> when democratically elected members of Myanmar's ruling party, <span>the National League for Democracy (NLD),</span> were deposed by the Tatmadaw Myanmar's military, which vested power in a stratocracy, the State Administration Council. The Tatmadaw declared a year-long state of emergency and declared power had been vested in Commander-in-Chief of Defence Services Min Aung Hlaing. </p> 
+                <p> The coup d'état occurred the day before the Parliament of Myanmar was due to swear in the members elected at the November 2020 general election, thereby preventing this from occurring. President Win Myint and State Counsellor Aung San Suu Kyi were detained, along with ministers and their deputies and members of Parliament.<br> The coup took place amid the COVID-19 pandemic. Myanmar was struggling with one of the most severe outbreaks in Southeast Asia, owing to its poor health infrastructure and ongoing internal conflict. The country had begun its COVID-19 vaccination deployment a few days before the coup. The country's economy had also been greatly affected by the pandemic, shrinking by 5% during 2020. </p> 
+            </div>
+            <div>
+                <img src="images/protest.jpg" alt="" width: "400px" height: "400px">
+           <p> 
+           <span>
+            Protesters have employed peaceful and nonviolent forms of protest, </span>which include acts of civil disobedience, labour strikes, a military boycott campaign, a pot-banging movement, a red ribbon campaign, public protests, and formal recognition of the election results by elected representatives. The <span style="color:red;">colour red,</span> which is associated with the <span> National League for Democracy (NLD),</span> has been donned by many protesters. "Kabar Ma Kyay Bu" (ကမ္ဘာမကျေဘူး), a song that was first popularize as the anthem of the 8888 Uprising, has been revitalised by the civil disobedience movement as a protest song. 
+            </p>
+            <img src="images/threefingers.jpg" alt="">
+            <p>
+            <span>The three-finger salute has been widely adopted by protesters as a protest symbol.</span>  <br> 
+            In response to the growing protest movement, the military leaders of the coup enacted a number of countermeasures. These include internet and social media blackouts, a media blackout, pursuit of arrests and criminal sentences against protesters, the spread of disinformation, political overtures to competing political parties to participate in the self-appointed State Administration Council (to replace the elected government body), deployment of pro-military protesters and instigators, and the violent use of force to suppress protests. As of 2 April 2021, at least 550 civilians, including children, have been killed by military or police forces and at least 2,574 people detained. 
+            On 2 February 2021, healthcare workers and civil servants across the country, including in the national capital, Naypyidaw, launched a national civil disobedience movement, in opposition to the coup d'état. 
+            </p>
+            <p>
+            Healthcare workers from dozens of state-run hospitals and institutions initiated a labour strike starting 3 February 2021. As of 3 February 2021, healthcare workers in over 110 hospitals and healthcare agencies[46] have participated in the movement. Labor strike participants have faced intimidation and threats from superiors. By 9 February, COVID vaccination had been suspended, the country's testing system had collapsed and most hospitals in Myanmar had shut down. <br> 
+            On 8 February, all of workers from Myanmar railways participated in the movement and so, the railway transportation is completely stopped. On 8 February, Kanbawza Bank temporarily closed its branches due to staffing shortages resulting from KBZ staff participating in the civil disobedience campaign. A number of industry lobbying groups, including the Myanmar Mobile Industry Association and the Myanmar Cosmetics Association, have suspended cooperation with government agencies following the coup.<br>
+            From 25 February, truckdrivers also began a strike against the coup by refusing to transport goods from the docks at Yangon’s four main ports. Joint secretary of the Myanmar Container Trucking Association said he estimates that about 90% of the 4,000 city's drivers are on strike, and have promised to deliver only essential food, medicine and fabrics for factories.  
+</p>
+</div>
+</div>
+<div class="footer"> <footer>All Credits Belong to its rightful owner. This is solely for educational purpose!</footer></div>`;
+var programsContent= ` <div class="programs">
+<h1>Myanmar: 2021 Civil Disobedience Movement</h1>
+<img src="images/protest3.jpg" alt="">
+<h2>Military boycott campaign</h2>
+<p><span> On 3 February 2021, a domestic boycott movement called the "Stop Buying Junta Business" campaign emerged,</span> calling for the boycott of products and services linked to the Myanmar military. Among the targeted goods and services in the Burmese military's significant business portfolio include Mytel, a national telecoms carrier, Myanmar Beer, Mandalay Beer, and Dagon Beer, several coffee and tea brands, 7th Sense Creation, which was co-founded by Min Aung Hlaing's daughter, and bus lines. 
+In response to the boycott, 71 engineers working for Mytel in Sagaing Region resigned in protest.Some retail outlets have begun pulling Myanmar Beer from stores. </p>
+<img src="images/pot-banging.jpg" alt="">
+<h2> Pot-banging movement</h2> 
+<p> <span>Since the onset of the coup d'état, residents in urban centers such as Yangon have staged cacerolazos, striking pots and pans in unison every evening</span> as a symbolic act to drive away evil, as a method of expressing their opposition to the coup d'état. On 5 February 2021, 30 people in Mandalay were charged under section 47 of the Police Act for banging pots and kitchenware.</p>
+<img src="images/healthcareworkers.webp" alt="">
+<h2>Red ribbon campaign<h2>
+<p><span>On 3 February 2021, healthcare workers in Myanmar launched the red ribbon campaign.</span> <span style="color:red;">The color red</span>  is associated with the National League for Democracy (NLD), the incumbent political party that won the 2020 elections.</p>
+<h2>Social media</h2>
+<p><span>Burmese netizens have popularised trending hashtags</span> like #SayNototheCoup, #RespectOurVotes, #HearTheVoiceofMyanmar, #SaveMyanmar, and #CivilDisobedience.[81][77] Within a day after the coup d'état, the #SaveMyanmar hashtag had been used by over 325,000 Facebook users. Social media users had also changed their profile pictures to black to show their sorrow or red in support of the NLD, often with a portrait of Suu Kyi. </p>
+<img src="images/motorcycle.jpg" alt="">
+<h2>Staged mass "car breakdowns" and "slow-car" movement </h2> 
+<p> <span></span>On 17 February 2021, many cars mysteriously broke down in busy streets of Yangon</span> in a staged demonstration, in a creative attempt to block security forces and police to pass through the traffic and to thwart the government workers from going to work. </p>
+<h2>Internet blackout</h2>  
+<p><span>
+On 4 February 2021, telecom operators and internet service providers across Myanmar were ordered to block Facebook until 7 February 2021, to ensure the "country's stability".</span>
+On the morning of 6 February 2021, the military authorities initiated an internet outage nationwide.That same day, Facebook urged authorities to unblock social media services. Facebook also removed the Burmese government's ability to submit content removal requests. Internet access was partially restored the following day, although social media platforms remained blocked. On 14 February, Telenor announced it was no longer permitted to publicly disclose the directives received from military authorities on internet disruptions. 
+Additional internet shutdowns were also reported on 22 and 26 February in some locations. Mytel, partly shared by the military, was seemingly not affected by the shutdowns. 
+On 1 April, the military council commanded to the wifi internet service providers to cut out wireless broadband internet too without reasons. </p>
+<h2>Arrests and charges</h2> 
+<p> Law enforcement authorities have acted swiftly in quelling opposition to the coup. <span>As of 7 February 2021, 152 people </span> were under detention in relation to the coup d'état.</p>
+</div>
+<div class="footer"> <footer>All Credits Belong to its rightful owner. This is solely for educational purpose!</footer></div>
+`;
+var aboutContent= ` <div class="about">
+<h1>The Story of General Aung San</h1>
+<img src="images/GeneralAungSan.jpg" alt="">
+<h3>Aung San</h3>
+<p> The Burmese political leader Aung San (1915-1947) was the driving force behind the nationalist movement that won Burma (now Myanmar) its freedom from British colonial rule in 1948. <br>
+Born in the township of Natmauk on Feb. 13, 1915, Aung San was the son of fairly well-off parents. He graduated from one of the high schools set up by Burmese nationalists to demonstrate their independence of foreign-provided education, and he received his bachelor's degree from the University of Rangoon. 
+Aung San was elected general secretary of the extreme nationalist Thakin (Our Own Masters) party in 1938, and he became the leading young nationalist before World War II and one of the two or three key Burmese political figures in the country. 
+</p>
+<h3>Fight for Independence </h3>
+<p> When Burmese "independence" was proclaimed in 1943, Aung San, who had been made a major general, was minister of war in the collaborationist Ba Maw government together with almost all of the other young nationalists. <br>
+In January 1947 Aung San, now Burma's acknowledged political leader, led the Burmese delegation to London for independence talks with British premier Clement Attlee. On his return in February 1947 Aung San successfully negotiated the Panglong Agreement, which provided for the participation of various frontier-area peoples in the new Union of Burma, as the emergent Burmese federal state was to be called. <br>
+On July 19, 1947—six months before the coming of independence—Aung San, only 32, and most of the other top nationalist leaders of the country were shot to death by henchmen of an insanely jealous political rival, prewar premier U Saw. The anniversary of the assassinations, known as Martyrs Day, is Myanmar's most solemn national holiday. </p>
+
+    <h1>Who is The Lady?</h1>
+    <img src="images/aungsansuukyi.jpg" alt="">
+    <p> Aung San Suu Kyi is the state counsellor of Myanmar and winner of the 1991 Nobel Prize for Peace.</p>
+    <p>
+    Born in Yangon, Myanmar, in 1945, Aung San Suu Kyi spent much of her early adult years abroad before returning home and becoming an activist against the brutal rule of dictator U Ne Win. She was placed under house arrest in 1989 and spent 15 of the next 21 years in custody, winning the 1991 Nobel Prize for Peace along the way. Suu Kyi was finally released from house arrest in November 2010 and subsequently held a seat in parliament for the National League for Democracy (NLD) party. Following the NLD's victory in 2016 parliamentary elections, Suu Kyi became the de facto head of the country in the new role of state counsellor. </p>
+    <img src="images/SuuKyi'sfamily.jpg" alt="">
+    <h3> Early Years</h3>
+    <p>
+    Aung San Suu Kyi was born on June 19, 1945, in Yangon, Myanmar, a country traditionally known as Burma. Her father, Generl Aung San, formerly the de facto prime minister of British Burma, was assassinated in 1947. In 1988, after Suu Kyi returned to Burma to care for her dying mother, her life took a dramatic turn.
+    </p>
+    <h3>Return to Burma</h3>
+    <p> In 1962, dictator U Ne Win staged a successful coup d'detat in Burma, which spurred intermittent protests over his policies during the subsequent decades. By 1988, he had resigned his post of party chairman, essentially leaving the country in the hands of a military junta, but stayed behind the scenes to orchestrate various violent responses to the continuing protests and other events. <br>
+    In 1988, when Suu Kyi returned to Burma from abroad, it was amid the slaughter of protesters rallying against U Ne Win and his iron-fisted rule. She soon began speaking out publicly against him, with issues of democracy and human rights at the fore of her agenda. It did not take long for the junta to notice her efforts, and in July 1989, the military government of Burma—which was renamed the Union of Myanmar—placed Suu Kyi under house arrest, cutting off any communication with the outside world. <br>
+    Suu Kyi was released from house arrest in July 1995, and the next year she attended the NLD party congress, under the continual harassment of the military. Three years later, she founded a representative committee and declared it the country's legitimate ruling body. In response, the junta in September 2000 once again placed her under house arrest. She was released in May 2002.
+    In 2003, the NLD clashed in the streets with pro-government demonstrators, and Suu Kyi was yet again arrested and placed under house confinement. Her sentence was then renewed every year, prompting the international community to call for her release.</p>
+    <h3>Arrest and Elections</h3>
+    <p>
+    In May 2009, just before she was set to be released from house arrest, Suu Kyi was arrested once more, this time charged with an actual crime—allowing an intruder to spend two nights at her home, a violation of her terms of house arrest. 
+    With Suu Kyi having won reelection as leader of her party in 2013, the country again held parliamentary elections on November 8, 2015, in what was viewed as the most open voting process in decades. Less than a week later, on November 13, the NLD was officially able to declare a landslide victory, having won 378 seats in a 664-seat parliament. <br>
+    In early March 2016, the party selected the country's new president, Htin Kyaw, who had been a longtime adviser to Suu Kyi. He was sworn in at the end of the month. Although Suu Kyi remained constitutionally barred from the presidency, in April 2016 the position of state counsellor was created to allow her a greater role in the country's affairs. Suu Kyi has publicly stated her intention to rule "above the president" until changes to the constitution can be addressed.</p>
+    <h3> Awards and Recognition</h3>
+   <p>
+    In 1991, Suu Kyi was awarded the Nobel Prize for Peace. She has also received the Rafto prize (1990), the International Simón Bolívar Prize (1992) and the Jawaharlal Nehru Award (1993), among other accolades.
+    In 2012, Suu Kyi was honored with the U.S. Holocaust Memorial Museum's Elie Wiesel Award, annually given to "internationally prominent individuals whose actions have advanced the Museum’s vision of a world where people confront hatred, prevent genocide, and promote human dignity," according to its website.</p>
+    <h3>Rohingya Persecution and Criticism</h3>
+    <p>
+    Not long after Suu Kyi's ascension to the role of state counsellor, the international community began looking into a series of escalating attacks on the Rohingya Muslims of Myanmar's coastal state of Rakhine. In October 2016, soldiers and civilian mobs banded together to terrorize and destroy Rohingya villages. A larger wave of violence erupted in August 2017, resulting in more than 600,000 Rohingya refugees fleeing across the border to Bangladesh.
+    In March 2018, the U.S. Holocaust Memorial Museum urged her to cooperate with international efforts "to establish the truth about the atrocities committed in Rakhine State and secure accountability for perpetrators" in her country. <br>
+    On 1 February 2021, Aung San Suu Kyi was arrested by the military during the 2021 Myanmar coup d'état after it declared the November 2020 Myanmar general election results fraudulent.</p>
+
+</div>
+<div class="footer"> <footer>All Credits Belong to its rightful owner. This is solely for educational purpose!</footer></div>`;
+var contactContent= ` <div class="contact">
+<div>
+<h1>Summary of What's happening</h1>
+<img src="images/protest2.jpg" alt="">
+<p>Democratically elected politicians and non-democratically elected military spokespersons meet in Naypyidaw to form a new government that will lead the country for the upcoming 5 years. <br>
+    What actually happened: <br>
+    1. Democratically elected politicians who were supposed to meet up, got kidnapped, by the military.  <br>
+    2. Citizens and public figures such as singers and writer got kidnapped for using their right of free speech to speak in favour of democracy. <br>
+    3. Phone signals got cut off around 3 am in Naypyidaw. <br>
+    4. The rest of the country followed. <br>
+    5. Tv, radio and some internet providers got cut off.<br>
+    6. Road to the airport got blocked. <br>
+    7. ATM’s and all banks are closed. <br>
+    8. MWD announced that Myint Swe is the new President <br>
+    9. The coup is official, and the country is in an emergency rule and the military will have full control over the country for at least one year. </p>
+</div>
+<div>
+    <img src="images/dr.sasa.jpg" alt=""> <br>
+    <a href="https://www.facebook.com/DrSasa22222/">DR Sasa, Special Envoy</a> <br>
+    <p>On 22 February 2021, he was appointed as CRPH's Special Envoy to the United Nations. A few days later, on 4 March 2021, he submitted a letter to the United Nations Security Council requesting the international body to honour its Responsibility to Protect (R2P) commitments with reference to the 2021 Myanmar protests and the violent military response to them.</p>
+</div>
+    <div>
+        <h2>Resources and Current Updates!</h2> <br>
+        <p>Please check these sites for more informations and to support the Civil Disobedience Movement</p>
+        <a href="https://www.facebook.com/theirrawaddy">The Irrawaddy</a> <br>
+        <a href="https://www.irrawaddy.com/donation?fbclid=IwAR0S1o-tjVowaJ5HHpqSS3lWkO15ybuc_Uv0OjgRzPrN3r9uYHp27-BRFKs">The Irrawaddy Donation</a> <br>
+        <a href="https://charity.gofundme.com/o/en/campaign/crph?fbclid=IwAR2JSCdEY6n8rW_HqP114OlhljIWzwxdgLlcJSHHiwtxEzk5cFlIgLNEqDU">GoFund Me</a><br>
+        <a href="https://www.facebook.com/worldwideburmesestudents/">World Wide Burmese Students</a>
+    </div>
+
+</div>`;
+
+//this is an example of a named function
+//inside the bracket will be an arguement
+function init(){
+// this is where the instruction go
+$("nav a").click(function(e) {
+    //this is a click listener that will get the link id
+    let btnId = this.id;
+    let contentID= btnId + "Content";
+    $("#app").html(eval(contentID));
+    // $("#app").append(btnId);
+    // $("#app").prepend(contentID);
+
+
+});
+}
+
+// Document/window means HTML File
+//this checks for all the elements to be read by the browser
+$(document).ready(function () {
+    init();
+});
